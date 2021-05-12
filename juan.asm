4204e783c64808b32e9e59a3077498a5d9c16eaa
Yusuf Çınar Özmen <marulumbenaq@gmail.com>
2021-05-12 16:26:17 +0300

    Add files via upload

---
+++ juan.asm
@@ -0,0 +1,19 @@
+section .data:
+    fikret: db "juan",10
+    fikret_length equ $-fikret
+section .text.
+    global main
+
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, fikret
+    mov rdx, fikret_length
+    syscall
+    call main
+
+exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
+    ret
